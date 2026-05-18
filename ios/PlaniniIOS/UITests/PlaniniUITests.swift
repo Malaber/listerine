d1be1b51c440439795d24ab9faeedad3dacdeff3
Malaber <git@schaedler.rocks>
2026-05-18 18:54:53 +0200

    test: submit iOS add item with return

--- ios/PlaniniIOS/UITests/PlaniniUITests.swift
+++ ios/PlaniniIOS/UITests/PlaniniUITests.swift
@@ -133,8 +133,7 @@ final class PlaniniUITests: XCTestCase {
 
         let nameField = app.textFields["add-item-name-field"]
         XCTAssertTrue(nameField.waitForExistence(timeout: 3))
-        nameField.typeText(enterSavedItemName)
-        tapElement(app.buttons["add-item-save-button"])
+        nameField.typeText("\(enterSavedItemName)\n")
         XCTAssertTrue(waitForElementToDisappear(app.otherElements["add-item-sheet"], timeout: 10))
         XCTAssertTrue(
             waitForItem(
